5a0a479245f85b59263106fc20eb709e6944905e
LorenzoLeonardini <dev.lorenzoleonardini@gmail.com>
2018-08-04 23:39:55 +0200

    Add UVa 10107
    What is the Median?

---
+++ UVa/10107 - What is the Median?/10107.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b)
+{
+	int A = *(int*)a;
+	int B = *(int*)b;
+	return A - B;
+}
+
+int main(void)
+{
+	long long int nums[10002];
+	int num = 0;
+
+	while(scanf("%lld", nums + num++) != EOF)
+	{
+		qsort(nums, num, sizeof(long long int), cmp);
+
+		if(num % 2 == 1)
+			printf("%lld\n", nums[num / 2]);
+		else
+			printf("%lld\n", (nums[num / 2 - 1] + nums[num / 2]) / 2);
+	}
+	return 0;
+}
